ebf65807ca8b3fcc28747dc6ff5f85a235d0acd3
HJK <han.junkwang@gmail.com>
2021-12-06 18:32:46 +0800

    Day 6.

---
+++ tests/dir/test06.nim
@@ -0,0 +1,8 @@
+import testutils
+import ../../src/day06
+
+let input = "input/day06.txt".readIntCsvLine
+
+input.part1.assertEquals 350605
+
+input.part2.assertEquals 1592778185024
--- tests/dir/testutils.nim
+++ tests/dir/testutils.nim
@@ -5,6 +5,8 @@ proc readStringLines*(path: string): seq[string] = path.readFile.splitLines
 
 proc readIntLines*(path: string): seq[int] = path.readStringLines.map(parseInt)
 
+proc readIntCsvLine*(path: string): seq[int] = path.readStringLines[0].split(',').map(parseInt)
+
 proc assertEquals*[T](actual: T, expected: T) =
   if actual != expected:
     echo "Expected ", expected, " but got ", actual
