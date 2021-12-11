0e1c3d4a0e3b9cbc8e1789864572e33a3ae9118a
HJK <han.junkwang@gmail.com>
2021-12-11 22:37:11 +0800

    Day 11 refactoring.

--- src/day11.nim
+++ src/day11.nim
@@ -1,27 +1,27 @@
 import adventutils
 import algorithm
 import sequtils
-import std/[deques, sets]
+import sets
 import sugar
 import tables
 
 func process(input: (IntGrid, int)): (IntGrid, int) =
   let grid = input[0]
   var
-    flashed = initHashSet[Point]()
+    flashed = 0
     newValues: Table[Point, int] = collect:
       for y in 0 ..< grid.len:
         for x in 0 ..< grid[0].len: {(x, y): grid[(x, y)] + 1}
-  while newValues.pairs.toSeq.anyIt(it[1] > 9 and not (it[0] in flashed)):
+  while newValues.pairs.toSeq.anyIt(it[1] > 9):
     for k in newValues.keys.toSeq.filter(p => newValues[p] > 9):
-      if not flashed.containsOrIncl(k):
-        for p in k.adjacents(grid, true): newValues[p] = newValues[p] + 1
-  for p in flashed: newValues[p] = 0
+      flashed.inc
+      newValues[k] = 0
+      for p in k.adjacents(grid, true).filter(a => newValues[a] > 0): newValues[p] += 1
   let newGrid = toSeq(0 ..< grid.len).foldl(
     a & newValues.pairs.toSeq.filterIt(it[0].y == b).sortedByIt(it[0].x).mapIt(it[1]),
     newSeq[seq[int]]()
   )
-  (newGrid, input[1] + flashed.len)
+  (newGrid, input[1] + flashed)
 
 func part1*(input: IntGrid): int = toSeq(1 .. 100).foldl(a.process, (input, 0))[1]
 
