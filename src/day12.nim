19b34b3ab4992be7556537c31924013261567a4e
HJK <han.junkwang@gmail.com>
2021-12-13 00:00:09 +0800

    Day 12 refactoring.

--- src/day12.nim
+++ src/day12.nim
@@ -12,15 +12,13 @@ func toMap(input: seq[string]): Table[string, seq[string]] =
     if y != "start": result.mgetOrPut(x, newSeq[string]()).add(y)
     if x != "start": result.mgetOrPut(y, newSeq[string]()).add(x)
 
-func process(input: seq[string], extraTest: (CountTable[string]) -> bool): int =
-  let caves = input.toMap
-  func explore(last: string, lastSeen: CountTable[string]): int =
-    if last == "end": 1 else:
-      var seen = lastSeen
-      if not last.isBigCave: seen.inc(last)
-      caves[last].filter(v => v.isBigCave or v notin seen or seen.extraTest).foldl(a + b.explore(seen), 0)
-  "start".explore(initCountTable[string]())
+func explore(caves: Table[string, seq[string]], revisits: int, last: string, lastSeen: CountTable[string]): int =
+  if last == "end": 1 else:
+    var seen = lastSeen
+    if not last.isBigCave: seen.inc(last)
+    caves[last].filter(v => v.isBigCave or v notin seen or revisits > 0)
+      .foldl(a + caves.explore((if b in seen: revisits - 1 else: revisits), b, seen), 0)
 
-func part1*(input: seq[string]): int = input.process(seen => false)
+func part1*(input: seq[string]): int = input.toMap.explore(0, "start", initCountTable[string]())
 
-func part2*(input: seq[string]): int = input.process(seen => seen.values.toSeq.foldl(a + b, 0) == seen.len)
+func part2*(input: seq[string]): int = input.toMap.explore(1, "start", initCountTable[string]())
