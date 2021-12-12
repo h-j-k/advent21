8b607606676b37926517247f4185b9c6a4a339ae
HJK <han.junkwang@gmail.com>
2021-12-12 20:20:40 +0800

    Day 12 part 2.

--- src/day12.nim
+++ src/day12.nim
@@ -9,19 +9,20 @@ func isBigCave(name: string): bool = name.items.toSeq.allIt(it.isUpperAscii)
 func toMap(input: seq[string]): Table[string, HashSet[string]] =
   for line in input:
     let (x, y) = (line.split("-")[0], line.split("-")[1])
-    result.mgetOrPut(x, initHashSet[string]()).incl(y)
-  let copy = result
-  for k, v in copy:
-    for x in v:
-      result.mgetOrPut(x, initHashSet[string]()).incl(k)
+    if y != "start": result.mgetOrPut(x, initHashSet[string]()).incl(y)
+    if x != "start": result.mgetOrPut(y, initHashSet[string]()).incl(x)
 
-func part1*(input: seq[string]): int =
+func process(input: seq[string], extraTest: (CountTable[string]) -> bool): int =
   let caves = input.toMap
-  var deque = [("start", newSeq[string](), initHashSet[string]())].toDeque
-  var paths = newSeq[seq[string]]()
+  var deque = [("start", newSeq[string](), initCountTable[string]())].toDeque
   while deque.len > 0:
     let (current, path, seen) = deque.popFirst
-    if current == "end": paths.add(path) else:
-      for next in caves[current].toSeq.filter(v => v.isBigCave or v notin seen):
-        deque.addLast((next, path & current, (if current.isBigCave: seen else: seen.union([current].toSet))))
-  paths.len
+    if current == "end": result.inc else:
+      var nextSeen = seen
+      if not current.isBigCave: nextSeen.inc(current)
+      for next in caves[current].toSeq.filter(v => v.isBigCave or v notin nextSeen or nextSeen.extraTest):
+        deque.addLast((next, path & current, nextSeen))
+
+func part1*(input: seq[string]): int = input.process(nextSeen => false)
+
+func part2*(input: seq[string]): int = input.process(nextSeen => nextSeen.values.toSeq.allIt(it == 1))
