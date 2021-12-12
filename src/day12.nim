9488ae6ae34c8e2dba5c036a812c390c81076cee
HJK <han.junkwang@gmail.com>
2021-12-13 00:04:46 +0800

    Day 12 refactoring.

--- src/day12.nim
+++ src/day12.nim
@@ -12,13 +12,13 @@ func toMap(input: seq[string]): Table[string, seq[string]] =
     if y != "start": result.mgetOrPut(x, newSeq[string]()).add(y)
     if x != "start": result.mgetOrPut(y, newSeq[string]()).add(x)
 
-func explore(caves: Table[string, seq[string]], revisits: int, last: string, lastSeen: CountTable[string]): int =
+func explore(caves: Table[string, seq[string]], revisits: int, last = "start", lastSeen = initCountTable[string]()): int =
   if last == "end": 1 else:
     var seen = lastSeen
     if not last.isBigCave: seen.inc(last)
     caves[last].filter(v => v.isBigCave or v notin seen or revisits > 0)
       .foldl(a + caves.explore((if b in seen: revisits - 1 else: revisits), b, seen), 0)
 
-func part1*(input: seq[string]): int = input.toMap.explore(0, "start", initCountTable[string]())
+func part1*(input: seq[string]): int = input.toMap.explore 0
 
-func part2*(input: seq[string]): int = input.toMap.explore(1, "start", initCountTable[string]())
+func part2*(input: seq[string]): int = input.toMap.explore 1
