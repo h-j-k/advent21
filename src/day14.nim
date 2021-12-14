dd6fe9389a2287bba817e3c0e2708294c1776990
HJK <han.junkwang@gmail.com>
2021-12-14 21:37:32 +0800

    Day 14 refactoring.

--- src/day14.nim
+++ src/day14.nim
@@ -10,7 +10,7 @@ func process(input: seq[string], times: int): int64 =
   let
     polymer = input[0]
     rules = input[2 ..< input.len].mapIt(it.scanTuple "$w -> $c").mapIt((it[1], it[2])).toTable
-    pairs = (1 .. times).foldl(a.process rules, polymer.zip(polymer[1 .. ^1]).mapIt(it[0] & it[1]).toCountTable)
+    pairs = (1 .. times).foldl(a.process rules, (1 ..< polymer.len).mapIt(polymer[(it - 1) .. it]).toCountTable)
     counts = (polymer[0] & polymer[^1]).newCountTable
   for k, v in pairs: (for c in k.items: counts.inc(c, v))
   let (min, max) = counts.values.toSeq.foldl((min(a[0], b), max(a[1], b)), (int64.high, int64.low))
