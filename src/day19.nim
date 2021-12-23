a781a3b98172e6dd8b484b6c78846dbe6aacf9a3
HJK <han.junkwang@gmail.com>
2021-12-24 01:33:34 +0800

    Day 19 refactoring.

--- src/day19.nim
+++ src/day19.nim
@@ -4,7 +4,6 @@ import math
 import sequtils
 import std/[deques, options, sets]
 import strscans
-import sugar
 import tables
 
 type Point3D = tuple[x: int, y: int, z: int]
@@ -21,31 +20,31 @@ type Mapping = tuple[xTo: char, yTo: char, zTo: char]
 
 type Delta = tuple[mx: int, my: int, mz: int, dx: int, dy: int, dz: int]
 
-func length(edge: Edge): int =
-  let (a, b) = edge
-  (a.x - b.x) ^ 2 + (a.y - b.y) ^ 2 + (a.z - b.z) ^ 2
-
 func length3(edge: Edge): (int, int, int) =
   let (a, b) = edge
   (abs(a.x - b.x), abs(a.y - b.y), abs(a.z - b.z))
 
-func sortedEdges(data: seq[Point3D]): seq[Edge] =
+func length(edge: Edge): int =
+  let (dx, dy, dz) = edge.length3
+  dx + dy + dz
+
+func toEdges(data: seq[Point3D]): seq[Edge] =
   var set = initHashSet[Edge]()
   for (i, d) in data[0 .. ^2].pairs:
     set = set + data[i + 1 .. ^1]
       .filterIt(abs(it.x - d.x) <= 1000 and abs(it.y - d.y) <= 1000 and abs(it.z - d.z) <= 1000)
       .foldl(a + [(a: d, b: b)].toHashSet, initHashSet[Edge]())
-  set.toSeq.sortedByIt(it.length)
+  set.toSeq
 
 func toData(data: string): Point3D =
   let (_, x, y, z) = data.scanTuple("$i,$i,$i")
   (x, y, z)
 
-func toData*(data: seq[string]): Scanner =
+func toData(data: seq[string]): Scanner =
   let
     (_, n) = data[0].scanTuple("--- scanner $i ---")
     data = data[1 .. ^1].foldl(a & b.toData, newSeq[Point3D]())
-  Scanner(data: data, edges: data.sortedEdges, n: [n].toHashSet)
+  Scanner(data: data, edges: data.toEdges, n: [n].toHashSet)
 
 func delta(mapping: Mapping, edge: Edge, reference: Edge): Delta =
   let
@@ -90,37 +89,35 @@ func delta(mapping: Mapping, edge: Edge, reference: Edge): Delta =
     else: discard
   (mx, my, mz, dx, dy, dz)
 
-func matches(e1: Edge, edges2: seq[Edge]): Table[(Mapping, Delta), HashSet[Point3D]] =
-  var t = initTable[(Mapping, Delta), HashSet[Point3D]]()
-  for e2 in edges2:
-    var mapping = none[Mapping]()
-    let
-      (de1x, de1y, de1z) = e1.length3
-      (de2x, de2y, de2z) = e2.length3
-    if de1x == 0 or de1y == 0 or de1z == 0: continue
-    if de1x == de2x:
-      if de1y == de2y and de1z == de2z: mapping = some((xTo: 'x', yTo: 'y', zTo: 'z'))
-      elif de1y == de2z and de1z == de2y: mapping = some((xTo: 'x', yTo: 'z', zTo: 'y'))
-    elif de1x == de2y:
-      if de1y == de2x and de1z == de2z: mapping = some((xTo: 'y', yTo: 'x', zTo: 'z'))
-      elif de1y == de2z and de1z == de2x: mapping = some((xTo: 'y', yTo: 'z', zTo: 'x'))
-    elif de1x == de2z:
-      if de1y == de2y and de1z == de2x: mapping = some((xTo: 'z', yTo: 'y', zTo: 'x'))
-      elif de1y == de2x and de1z == de2y: mapping = some((xTo: 'z', yTo: 'x', zTo: 'y'))
-    if mapping.isSome:
-      let k = (mapping.get, delta(mapping.get, e1, e2))
-      t[k] = (t.getOrDefault(k, initHashSet[Point3D]()) + [e1.a, e1.b].toHashSet)
-  t
+func matches(e1: Edge, e2: Edge): Table[(Mapping, Delta), HashSet[Point3D]] =
+  var mapping = none[Mapping]()
+  let
+    (de1x, de1y, de1z) = e1.length3
+    (de2x, de2y, de2z) = e2.length3
+  if de1x == 0 or de1y == 0 or de1z == 0: continue
+  if de1x == de2x:
+    if de1y == de2y and de1z == de2z: mapping = some((xTo: 'x', yTo: 'y', zTo: 'z'))
+    elif de1y == de2z and de1z == de2y: mapping = some((xTo: 'x', yTo: 'z', zTo: 'y'))
+  elif de1x == de2y:
+    if de1y == de2x and de1z == de2z: mapping = some((xTo: 'y', yTo: 'x', zTo: 'z'))
+    elif de1y == de2z and de1z == de2x: mapping = some((xTo: 'y', yTo: 'z', zTo: 'x'))
+  elif de1x == de2z:
+    if de1y == de2y and de1z == de2x: mapping = some((xTo: 'z', yTo: 'y', zTo: 'x'))
+    elif de1y == de2x and de1z == de2y: mapping = some((xTo: 'z', yTo: 'x', zTo: 'y'))
+  if mapping.isSome:
+    let k = (mapping.get, delta(mapping.get, e1, e2))
+    result[k] = (result.getOrDefault(k, initHashSet[Point3D]()) + [e1.a, e1.b].toHashSet)
 
 func deltaRelativeTo(s1: Scanner, s2: Scanner): Option[(Mapping, Delta)] =
   var mappings = initTable[(Mapping, Delta), HashSet[Point3D]]()
   for e1 in s1.edges:
-    for (k, v) in e1.matches(s2.edges).pairs:
-      mappings[k] = mappings.getOrDefault(k, initHashSet[Point3D]()) + v
+    for e2 in s2.edges:
+      for (k, v) in e1.matches(e2).pairs:
+        mappings[k] = mappings.getOrDefault(k, initHashSet[Point3D]()) + v
   let candidates = mappings.pairs.toSeq.filterIt(it[1].len > 2).sortedByIt(-1 * it[1].len)
   return if candidates.len > 0: some(candidates[0][0]) else: none[(Mapping, Delta)]()
 
-func offset(mapping: Mapping, delta: Delta, p: Point3D): Point3D =
+func offset(p: Point3D, mapping: Mapping, delta: Delta): Point3D =
   let
     (xTo, yTo, zTo) = mapping
     (mx, my, mz, dx, dy, dz) = delta
@@ -148,7 +145,7 @@ func offset(mapping: Mapping, delta: Delta, p: Point3D): Point3D =
 func toOffset(mapping: Mapping, delta: Delta): Point3D =
   let
     (xTo, yTo, zTo) = mapping
-    (mx, my, mz, dx, dy, dz) = delta
+    (_, _, _, dx, dy, dz) = delta
   var x, y, z: int
   case xTo:
     of 'x': x = dx
@@ -173,8 +170,8 @@ func `+`(sAcc: Scanner, s: Scanner): (Scanner, Option[Point3D]) =
   if r.isNone: return (sAcc, none[Point3D]())
   let
     (mapping, delta) = r.get
-    newData = (sAcc.data & s.data.mapIt(offset(mapping, delta, it))).deduplicate
-  (Scanner(data: newData, edges: newData.sortedEdges, n: (sAcc.n + s.n)), some(mapping.toOffset delta))
+    newData = (sAcc.data & s.data.mapIt(it.offset(mapping, delta))).deduplicate
+  (Scanner(data: newData, edges: newData.toEdges, n: (sAcc.n + s.n)), some(mapping.toOffset delta))
 
 func process(input: seq[string]): (Scanner, seq[Point3D]) =
   let scanners = input.splitByEmptyLines.mapIt(it.toData)
@@ -185,20 +182,15 @@ func process(input: seq[string]): (Scanner, seq[Point3D]) =
   while remaining.len > 0:
     let
       next = remaining.popFirst
-      (r, offset) = combined + next
-    if offset.isSome:
-      combined = r
-      offsets.add offset.get
-    else:
-      remaining.addLast next
+      (updated, offset) = combined + next
+    combined = updated
+    if offset.isSome: offsets.add offset.get else: remaining.addLast next
   (combined, offsets)
 
 func part1*(input: seq[string]): int = input.process[0].data.len
-
-func distance(p1: Point3D, p2: Point3D): int = abs(p1.x - p2.x) + abs(p1.y - p2.y) + abs(p1.z - p2.z)
 
 func part2*(input: seq[string]): int =
   let (_, offsets) = input.process
   result = 0
   for (i, offset) in offsets[0 .. ^2].pairs:
-    result = offsets[i + 1 .. ^1].foldl(a.max offset.distance(b), result)
+    result = offsets[i + 1 .. ^1].foldl(a.max (offset, b).length, result)
